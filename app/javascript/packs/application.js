389bab41c0a7b96aa529ce216f22924db418e537
Mark Rebec <mark@markrebec.com>
2019-09-09 16:05:17 -0700

    packages bootstrap and reactstrap for the react app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from 'components/App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(<App />, document.body.appendChild(document.createElement('div')))
