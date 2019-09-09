629cd27148c3aa585751470159097817c157fc1c
Mark Rebec <mark@markrebec.com>
2019-09-09 12:21:23 -0700

    adds mutations to delete blobs and gists

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -18,6 +18,21 @@ module Types
       raise GraphQL::ExecutionError.new("Failed to create gist", extensions: result.gist.errors)
     end
 
+    field :delete_gist, Types::GistType, null: false do
+      description "Delete a gist"
+      argument :id, ID, required: true
+    end
+
+    def delete_gist(id:)
+      raise GraphQL::ExecutionError.new("You must be logged in to delete a gist") unless context[:current_user].present?
+      gist = Gist.find(id)
+      # TODO should also use access control like pundit or cancan for this sorta stuff
+      raise GraphQL::ExecutionError.new("You are not allowed to delete this gist") unless context[:current_user] == gist.user
+
+      gist.destroy
+      gist
+    end
+
     field :create_blob, Types::BlobType, null: false do
       description "Create a new blob within an existing gist"
       argument :gist_id, ID, required: true
@@ -28,12 +43,25 @@ module Types
     def create_blob(gist_id:, **args)
       raise GraphQL::ExecutionError.new("You must be logged in to create a blob") unless context[:current_user].present?
       gist = Gist.find(gist_id)
-      # TODO should also use access control like pundit or cancan for this sorta stuff
       raise GraphQL::ExecutionError.new("You are not allowed to edit this gist") unless context[:current_user] == gist.user
 
       result = CreateBlob.call(gist: gist, **args)
       return result.blob if result.success?
       raise GraphQL::ExecutionError.new("Failed to create blob", extensions: result.blob.errors)
+    end
+
+    field :delete_blob, Types::BlobType, null: false do
+      description "Delete a blob"
+      argument :id, ID, required: true
+    end
+
+    def delete_blob(id:)
+      raise GraphQL::ExecutionError.new("You must be logged in to delete a blob") unless context[:current_user].present?
+      blob = Blob.find(id)
+      raise GraphQL::ExecutionError.new("You are not allowed to delete this blob") unless context[:current_user] == blob.user
+
+      blob.destroy
+      blob
     end
 
   end
