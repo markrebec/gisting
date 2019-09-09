f9c03630d92c7de845a4cbbf7ec2e9f95fd518d4
Mark Rebec <mark@markrebec.com>
2019-09-09 15:27:24 -0700

    exposes gist owner in graphql types

--- app/graphql/types/gist_type.rb
+++ app/graphql/types/gist_type.rb
@@ -5,6 +5,7 @@ module Types
     field :privacy, String, null: false
     field :description, String, null: true
     field :blobs, [Types::BlobType], null: true
+    field :user, Types::UserType, null: false
     field :created_at, String, null: true
     field :updated_at, String, null: true
   end
