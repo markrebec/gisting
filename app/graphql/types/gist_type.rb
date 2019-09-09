a03d4a4fce88097fee85a9f19f1f2832a4f903b4
Mark Rebec <mark@markrebec.com>
2019-09-09 12:32:55 -0700

    adds privacy field to gist type

--- app/graphql/types/gist_type.rb
+++ app/graphql/types/gist_type.rb
@@ -2,6 +2,7 @@ module Types
   class GistType < BaseObject
     description "A gist"
     field :id, ID, null: false
+    field :privacy, String, null: false
     field :description, String, null: true
     field :blobs, [Types::BlobType], null: true
   end
