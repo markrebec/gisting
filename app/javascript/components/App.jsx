d88d0a6b1a1136ce32c30d5819809c8d240193d1
Mark Rebec <mark@markrebec.com>
2019-09-09 16:20:22 -0700

    packages and hooks up react router

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import { ApolloProvider } from '@apollo/react-hooks'
 import apollo from 'utilities/apollo'
 import Home from 'components/Home'
 
 export default props => <ApolloProvider client={apollo}>
-  <Home />
+  <Router>
+    <Route path="/" exact component={Home} />
+  </Router>
 </ApolloProvider>
