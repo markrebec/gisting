de82d6248d629d5450b7d941ad6b61ef3f92c193
Mark Rebec <mark@markrebec.com>
2019-09-09 16:37:43 -0700

    creates a route component for gists

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,11 +4,13 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import apollo from 'utilities/apollo'
 import NavBar from 'components/NavBar'
 import Home from 'components/Home'
+import Gists from 'components/Gists'
 
 export default props => <ApolloProvider client={apollo}>
   <Router>
     <NavBar />
 
     <Route path="/" exact component={Home} />
+    <Route path="/gists" exact component={Gists} />
   </Router>
 </ApolloProvider>
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,33 +1,3 @@
 import React from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import { gql } from 'apollo-boost'
-import Gist from 'components/Gist'
 
-export default props => {
-  const { loading, error, data } = useQuery(gql`
-    {
-      gists {
-        id
-        description
-        privacy
-        createdAt
-        updatedAt
-        user {
-          username
-        }
-        blobs {
-          id
-          filename
-          body
-          createdAt
-          updatedAt
-        }
-      }
-    }
-  `)
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error!</p>
-
-  return data.gists.map(gist => <Gist key={gist.id} gist={gist} />)
-}
+export default props => <h1>Gisting</h1>
