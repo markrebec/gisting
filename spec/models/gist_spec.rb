45fbdc6da2b316c71e9b0500c0a39e43c7608d97
Mark Rebec <mark@markrebec.com>
2019-09-09 13:31:46 -0700

    removes pending model specs but leaves files in place for potential future logic

--- spec/models/gist_spec.rb
+++ spec/models/gist_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Gist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # no custom functionality, just standard activerecord
 end
