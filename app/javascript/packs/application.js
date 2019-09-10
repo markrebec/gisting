4c40a4e71bfbecfaba4aa683ef2c1f2c23195ffb
Mark Rebec <mark@markrebec.com>
2019-09-09 21:32:08 -0700

    packages highlight.js and includes the github-gist theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,6 +19,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from 'components/App'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'highlight.js/styles/github-gist.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(<App />, document.body.appendChild(document.createElement('div')))
