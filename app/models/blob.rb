1e6f90fa1a677b96867d937d854d882214b218bc
Mark Rebec <mark@markrebec.com>
2019-09-10 11:05:08 -0700

    adds chronological scope to blobs

--- app/models/blob.rb
+++ app/models/blob.rb
@@ -4,5 +4,7 @@ class Blob < ApplicationRecord
   belongs_to :gist
   has_one :user, through: :gist
 
+  scope :chronological, -> { order('created_at ASC, id ASC') }
+
   audited associated_with: :gist
 end
