38c3a2bd57f726de799c829924a0014d12a60e82
Mark Rebec <mark@markrebec.com>
2019-09-09 18:11:49 -0700

    adds field indicating ownership to gist type

--- app/graphql/types/gist_type.rb
+++ app/graphql/types/gist_type.rb
@@ -8,17 +8,21 @@ module Types
     field :user, Types::UserType, null: false
     field :created_at, String, null: true
     field :updated_at, String, null: true
-
+    field :is_owner, Boolean, null: false
     field :blob, Types::BlobType, null: true do
       argument :id, ID, required: true
     end
 
-    def blob(id:)
-      object.blobs.find(id)
-    end
-
     def description
       object.description || object.blobs.first.try(:filename)
+    end
+
+    def is_owner
+      object.user == context[:current_user]
+    end
+
+    def blob(id:)
+      object.blobs.find(id)
     end
   end
 end
