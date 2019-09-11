8188516932614ec51e18441332aaefae0d7d1d88
Mark Rebec <mark@markrebec.com>
2019-09-11 11:14:46 -0700

    wraps homepage component with basic auth query

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+import meQuery from 'queries/me'
 import { Jumbotron, Button } from 'reactstrap'
 
-export default props => <Jumbotron>
-  <h1>Welcome!</h1>
-  <p className="lead">Use the buttons below to sign in or sign up and start creating gists.</p>
-  <hr className="my-2" />
-  <p className="lead text-center">
-    <Button color="primary" tag="a" href="/users/sign_in">Sign in</Button>
-    <span className="text-muted"> or </span>
-    <Button color="primary" tag="a" href="/users/sign_up">Sign up</Button>
-  </p>
-</Jumbotron>
+export default props => {
+  const { loading, error, data } = useQuery(meQuery)
+
+  if (loading) return null
+
+  if (error || data.me == null) {
+    return <Jumbotron>
+      <h1>Welcome!</h1>
+      <p className="lead">Use the buttons below to sign in or sign up and start creating gists.</p>
+      <hr className="my-2" />
+      <p className="lead text-center">
+        <Button color="primary" tag="a" href="/users/sign_in">Sign in</Button>
+        <span className="text-muted"> or </span>
+        <Button color="primary" tag="a" href="/users/sign_up">Sign up</Button>
+      </p>
+    </Jumbotron>
+  } else {
+    return <h1>CREATE GIST FORM PLACEHOLDER</h1>
+  }
+}
