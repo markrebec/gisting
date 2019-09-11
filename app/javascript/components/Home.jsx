c8ed1167d0a4ee4e481487b728c229aa52ca8a10
Mark Rebec <mark@markrebec.com>
2019-09-11 11:04:49 -0700

    adds jumbotron with sign in/up links to homepage

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,3 +1,13 @@
 import React from 'react'
+import { Jumbotron, Button } from 'reactstrap'
 
-export default props => <h1>Gisting</h1>
+export default props => <Jumbotron>
+  <h1>Welcome!</h1>
+  <p className="lead">Use the buttons below to sign in or sign up and start creating gists.</p>
+  <hr className="my-2" />
+  <p className="lead text-center">
+    <Button color="primary" tag="a" href="/users/sign_in">Sign in</Button>
+    <span className="text-muted"> or </span>
+    <Button color="primary" tag="a" href="/users/sign_up">Sign up</Button>
+  </p>
+</Jumbotron>
